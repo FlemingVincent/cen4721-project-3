d9fbc0e6631acf46026c4ddd1528983652d11d04
Vincent <flemingvincent@ufl.edu>
2022-07-26 10:38:50 -0400

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHQ8SysCWiY0jWmaT3NZwLCHOl1W3GwFY",
+  authDomain: "cen4721-project-3.firebaseapp.com",
+  projectId: "cen4721-project-3",
+  storageBucket: "cen4721-project-3.appspot.com",
+  messagingSenderId: "361789104908",
+  appId: "1:361789104908:web:a3cb3890356bed59b65f29",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
