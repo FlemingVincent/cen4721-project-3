eb9a7ae99732cf6382d0a2e1e92ea05dfb462b36
Yiru Mu <yiru.mu@ufl.edu>
2022-07-27 11:47:15 -0400

    added side bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div>
         <Header/>
         {/*App body*/}
-            {/*Side Bar*/}
-            {/*Posts*/}
-
+        <div className="app_body">
+            {/*Posts Note: make the post area position:sticky flex:0.8 side bar would be pushed to the right*/}
+            <Sidebar/>
+        </div>
     </div>
   );
 }
