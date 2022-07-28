726cd8208e7559ad2dc0d0251594642e52c731f9
shravyairuvanti <ishravya01@gmail.com>
2022-07-28 19:17:06 -0400

    register not working

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {useSelector} from "react-redux";
 
 function App() {
     const user = useSelector(selectUser);
-
+  
 
   return (
     <div classsName="app">
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "./features/counter/userSlice";
+import { auth } from "./firebase";
 import "./Login.css";
 
 function Login() {
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [name, setName] = useState("");
+const [profilePic, setProfilePic] = useState("");
+const dispatch = useDispatch();
+    const loginToApp = (e) => {
+        e.preventDefault();
+    };
+        
+    const register = () => {
+        if (!name) {
+            return alert("Please enter a full name!");
+        }
+
+        auth.currentUserWithEmailAndPassword(email,password)
+        .then((userAuth) => {
+            userAuth.user.updateProfile({
+                displayName: name,
+                photoURL: profilePic,
+            })
+            .then(() =>{
+                dispatch(login({
+                    email: userAuth.user.email,
+                    uid: userAuth.user.uid,
+                    displayName: name,
+                    photoUrl: profilePic
+                }));
+            });
+        })
+        .catch(error => alert(error));
+    };
     return (
     <div className="login">
-        <h1>You are not logged in</h1>
+        <img 
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/1/14/Florida_Gators_gator_logo.svg/1200px-Florida_Gators_gator_logo.svg.png" 
+            alt=""
+        />
+        <form>
+            <input 
+            value={name} 
+            onChange={(e)=> setName(e.target.value)} 
+            placeholder="Full name (required if registering)"
+            type="text" />
+
+            <input 
+            value={profilePic} 
+            onChange={(e)=> setProfilePic(e.target.value)}
+            placeholder="Profile pic URL (optional)" 
+            type="text" />
+
+            <input 
+                value={email} 
+                onChange={(e)=> setEmail(e.target.value)} 
+                placeholder="Email" 
+                type="email" 
+            />
+            <input 
+                value={password} 
+                onChange={(e)=> setPassword(e.target.value)}
+                placeholder="Password" 
+                type="password" 
+            />
+
+            <button type='submit' onClick={loginToApp}>Sign In</button>
+            
+        </form>
+
+        <p>Not a member?{" "}
+            <span className ="login_register" onClick={register}
+            >Register Now </span>
+        </p>
     </div>
     );
 }
